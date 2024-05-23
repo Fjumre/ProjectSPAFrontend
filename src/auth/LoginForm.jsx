b55b3ef59e24de366a4d1bb9aae965217e189a77
MGold <mikkelgold@protonmail.com>
2024-05-23 14:09:21 +0200

    ProjectFrontend

--- src/auth/LoginForm.jsx
+++ src/auth/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import login from '../services/apiFacade';
-import 'landscaping.css'
+import './landscaping.css'; 
 
 
 const LoginForm = () => {
