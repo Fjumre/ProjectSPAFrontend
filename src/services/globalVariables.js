56beefcc257c54ccf1a4e50723c5c64c17351e24
MGold <mikkelgold@protonmail.com>
2024-05-31 21:43:08 +0200

    frontend

--- src/services/globalVariables.js
+++ src/services/globalVariables.js
@@ -1,3 +1,3 @@
-export const PRODUCTION_API_BASE_URL = "http://localhost:7070/api"
+//export const PRODUCTION_API_BASE_URL = "http://localhost:7070/api"
 
-//export const PRODUCTION_API_BASE_URL = "https://framj.com:7070/api";
+export const PRODUCTION_API_BASE_URL = "https://framj.com:7070/api";
